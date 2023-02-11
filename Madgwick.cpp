4f1a8ce0210afeb2ef387d32d6041768ef1d947f
Satoshi Ohya <conatus.11099@gmail.com>
2023-02-11 16:20:53 +0900

    first commit

---
+++ Madgwick.cpp
@@ -0,0 +1,8 @@
+/*
+ * Magdwick.cpp
+ *
+ *  Created on: Feb 11, 2023
+ *      Author: OHYA Satoshi
+ */
+
+#include "Madgwick.h"
---
+++ Madgwick.h
@@ -0,0 +1,18 @@
+/*
+ * Magdwick.h
+ *
+ *  Created on: Feb 11, 2023
+ *      Author: OHYA Satoshi
+ */
+
+#ifndef MAGDWICK_MADGWICK_H_
+#define MAGDWICK_MADGWICK_H_
+
+class Madgwick {
+public:
+	Madgwick();
+
+
+};
+
+#endif /* MAGDWICK_MADGWICK_H_ */
