742475cbbcbc219961f561e0025a4b4795a315b3
Satoshi Ohya <conatus.11099@gmail.com>
2023-02-11 19:03:33 +0900

    filter本体の定義

--- Madgwick.cpp
+++ Madgwick.cpp
@@ -6,3 +6,45 @@
  */
 
 #include "Madgwick.h"
+
+void Madgwick::update(
+		std::array<float, 3> accelValue,
+		std::array<float, 3> gyroValue,
+		float time)
+{
+	float stepTime = time - befTime;
+	befTime = time;
+
+	Quaternion qDotOmega;
+	std::array<float,3> f;
+	std::array<std::array<float,3>,4> j;
+	Quaternion qDotEpsilon;
+	Quaternion qDot;
+	const float beta=std::sqrt(3/4.0)*M_PI*(5.0/180.0);
+
+	Quaternion gyroQuaternion(gyroValue);
+
+	qDotOmega = quaternion * gyroQuaternion;
+	qDotOmega *= 0.5;
+
+	f[0]=2*(quaternion[1]*quaternion[3]-quaternion[0]*quaternion[2])-accelValue[0];
+	f[1]=2*(quaternion[0]*quaternion[1]+quaternion[2]*quaternion[3]-accelValue[1]);
+	f[2]=2*(1/2.0-std::pow(quaternion[1],2)-std::pow(quaternion[2],2))-accelValue[2];
+
+	j[0]=  {-2*quaternion[2], 2*quaternion[1],0};
+	j[1]=  {2*quaternion[3],2*quaternion[0],-4*quaternion[1]};
+	j[2]=  {-2*quaternion[0],2*quaternion[3],-4*quaternion[2]};
+	j[3]=  {2*quaternion[1],2*quaternion[2],0};
+
+	 for(uint8_t n=0;n<4;n++){
+		 qDotEpsilon[n]=0;
+		 for(uint8_t m=0;m<3;m++){
+			 qDotEpsilon[n]+= j[n][m]*f[m];
+		 }
+	 }
+
+	 qDot = qDotOmega - qDotEpsilon.normalize()*beta;
+	 quaternion=quaternion+qDot*stepTime/1000.0;
+
+	 quaternion.normalize();
+}
--- Madgwick.h
+++ Madgwick.h
@@ -8,11 +8,26 @@
 #ifndef MAGDWICK_MADGWICK_H_
 #define MAGDWICK_MADGWICK_H_
 
+#include <cmath>
+#include <array>
+#include "Quaternion/Quaternion.h"
+
+
 class Madgwick {
 public:
 	Madgwick();
 
+	/* brief calculate quaternion
+	 * param accelValue Value of accele meter (m/s^2)
+	 * param gyroValue Value of gyroscope (rad/s)
+	 * param time Time since start program (ms)
+	 */
+	void update(std::array<float, 3> accelValue, std::array<float, 3> gyroValue, float time);
 
+
+private:
+	Quaternion quaternion;
+	float befTime;
 };
 
 #endif /* MAGDWICK_MADGWICK_H_ */
